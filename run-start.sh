d486edcbf12030f6e18eee17d6172b8fa799b091
markshawn2020 <shawninjuly@gmail.com>
2023-04-10 07:06:16 +0800

    chore: updated env scripts

--- run-start.sh
+++ run-start.sh
@@ -2,7 +2,7 @@ if [ "$ENV" == "alpha" ]; then
   git stash && git pull -f && yarn
   yarn build && yarn start-alpha
 elif [ "$ENV" == "prod" ]; then
-  yarn start;
+  yarn start-prod
 else
   yarn dev
 fi
