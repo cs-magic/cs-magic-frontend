8626dfd2a85e765922900a62f6b5740188d4d302
markshawn2020 <shawninjuly@gmail.com>
2023-04-10 07:36:28 +0800

    chore: updated env scripts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+	reactStrictMode: false,
+	distDir: '.' + (process.env.ENV || 'next')
 }
 
 module.exports = nextConfig
