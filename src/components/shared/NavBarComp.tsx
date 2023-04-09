3eeff1032132779b1cd53ec264979ed84331128f
markshawn2020 <shawninjuly@gmail.com>
2023-04-09 23:38:27 +0800

    ui: better title

--- src/components/shared/NavBarComp.tsx
+++ src/components/shared/NavBarComp.tsx
@@ -5,35 +5,33 @@ import { NavbarAvatarComp } from '@/components/shared/NavbarAvatarComp'
 export const NavBarComp = ({ title }: { title: string }) => {
 	
 	return (
-		<div className="navbar bg-base-100">
-			<div className="navbar-start">
+		<div className="inline-flex items-center w-full bg-base-100 px-2">
+			
+			<DropdownMenu>
 				
-				<DropdownMenu>
-					
-					<DropdownMenuTrigger>
-						<label tabIndex={0} className="btn btn-ghost btn-circle">
-							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7"/>
-							</svg>
-						</label>
-					</DropdownMenuTrigger>
-					
-					<DropdownMenuContent>
-						<DropdownMenuItem asChild><Link href={'/'} className={'cursor-pointer'}>HomePage</Link></DropdownMenuItem>
-						{/*<DropdownMenuItem>About</DropdownMenuItem>*/}
-					</DropdownMenuContent>
+				<DropdownMenuTrigger>
+					<label tabIndex={0} className="btn btn-ghost btn-circle">
+						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7"/>
+						</svg>
+					</label>
+				</DropdownMenuTrigger>
 				
-				</DropdownMenu>
+				<DropdownMenuContent>
+					<DropdownMenuItem asChild><Link href={'/'} className={'cursor-pointer'}>HomePage</Link></DropdownMenuItem>
+					{/*<DropdownMenuItem>About</DropdownMenuItem>*/}
+				</DropdownMenuContent>
 			
+			</DropdownMenu>
+			
+			<div className="btn btn-ghost normal-case text-xl flex-1 truncate">
+				<p className={'w-full truncate inline-flex justify-center items-center'}>
+					<span className={'hidden md:block'}>CS魔法社 |&nbsp;</span>
+					<span>{title}</span>
+				</p>
 			</div>
 			
-			<div className="navbar-center">
-				<a className="btn btn-ghost normal-case text-xl">CS魔法社 | {title}</a>
-			</div>
-			
-			<div className={'navbar-end'}>
-				<NavbarAvatarComp/>
-			</div>
+			<NavbarAvatarComp/>
 		</div>
 	)
 }
