959905abb182c35a7cac111b58cef81694a29244
markshawn2020 <shawninjuly@gmail.com>
2023-04-10 13:26:45 +0800

    test backend endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { BACKEND_ENDPOINT } from '@/lib/env'
 
 
+console.log({ BACKEND_ENDPOINT })
 // Set config defaults when creating the instance
 const instance = axios.create({
 	baseURL: BACKEND_ENDPOINT,
