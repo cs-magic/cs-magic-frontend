8de59031e0d7d4db14a16ddffc4be6754daad24a
markshawn2020 <shawninjuly@gmail.com>
2023-04-12 18:41:03 +0800

    updated env of callback url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,9 @@
 export const BACKEND_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT
 export const FPJS_API_KEY = process.env.NEXT_PUBLIC_FPJS_API_KEY
-export const NEXTAUTH_CALLBACK_URL = process.env.NEXT_PUBLIC_NEXTAUTH_CALLBACK_URL
+console.log('env: ', process.env)
+export const NEXTAUTH_CALLBACK_URL = process.env.ENV === 'prod'
+	? 'https://cs-magic.com'
+	: process.env.ENV === 'alpha'
+		? 'https://alpha.cs-magic.com'
+		: 'http://localhost:2002'
+		
