c9391380aa826c0daf1c74da367ca004ac396f07
markshawn2020 <shawninjuly@gmail.com>
2023-04-21 07:57:51 +0800

    fixed clotho image

--- src/components/views/ContactView.tsx
+++ src/components/views/ContactView.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export const ContactView = () => {
 	return (
-		<Image src={'/qrcodes/wechat-clotho.jpeg'} alt={'contact'} width={320} height={480}/>
+		<Image src={'/qrcodes/wechat-clotho.png'} alt={'contact'} width={320} height={480}/>
 	)
 }
