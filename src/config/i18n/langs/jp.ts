d38bc1f5d40da8897486f3ab71d57febf6211adb
clotho233 <51307404+clotho233@users.noreply.github.com>
2023-04-22 11:38:52 +0900

    Update jp.ts

--- src/config/i18n/langs/jp.ts
+++ src/config/i18n/langs/jp.ts
@@ -2,28 +2,28 @@ import { II18nSchema } from '@/config/i18n/schema'
 
 export const jp: II18nSchema = {
 	hero: {
-		title: '欢迎来到CS魔法社',
-		subtitle: '基于计算机技术的一些研究、分享，包括基于OpenAI的chatGPT、Dalle模型等',
-		entrance: '开启您的新世界',
+		title: 'CSマジッククラブへようこそ。',
+		subtitle: 'これは、コンピュータ技術をベースにした研究・共有のためのコミュニティです。OpenAIをベースにしたchatGPT、Dalleなどを含みます。',
+		entrance: 'あなたの新しい世界を切り開こう。',
 	},
 	website: {
-		platformName: 'CS魔法社',
-		avatarPlaceholder: '登录',
+		platformName: 'CSマジッククラブ',
+		avatarPlaceholder: 'ログイン',
 	},
 	
 	projects: {
 		chatGPT: {
-			name: 'ChatGPT 聊天',
-			desc: '基于OpenAI的ChatGPT，支持与AI连续对话',
+			name: 'ChatGPT チャット',
+			desc: 'OpenAIをベースにしたChatGPT、AIとの継続的な対話をサポートできます。',
 			href: '/apps/chat/chatGPT',
-			tags: ['NEW', '稳定', '免翻'],
+			tags: ['新しい', '安定', 'ミラーサイト'],
 			cover: '/screenshots/chatgpt.png',
 		},
 		dalle: {
-			name: 'Dalle 作图',
-			desc: '基于OpenAI的Dalle，支持根据文本生成图片',
+			name: 'Dalle 画像を作成',
+			desc: 'OpenAIをベースにしたDalle、テキストからの画像生成に対応できます。',
 			href: '/apps/chat/dalle',
-			tags: ['NEW', '稳定', '免翻'],
+			tags: ['新しい', '安定', 'ミラーサイト'],
 			cover: '/screenshots/dalle2.png',
 		},
 	},
@@ -31,90 +31,90 @@ export const jp: II18nSchema = {
 	abouts: {
 		aboutUS: {
 			href: '/about/us',
-			name: '关于我们',
+			name: '開発者について',
 		},
 		userPlanning: {
 			href: '/user-planning',
-			name: '会员计划'
+			name: '会員制度'
 		},
 		// contact: {
 		// 	href: '/about/contact',
-		// 	name: '联系我们',
+		// 	name: 'お問い合わせ先',
 		// },
 		jobs: {
 			href: '/about/occupation',
-			name: '职业机会',
+			name: '就職希望',
 		},
 	},
 	
 	legals: {
 		termOfUse: {
 			href: '/legal/term-of-use',
-			name: '适用条款',
+			name: '適用条件',
 		},
 		privacyPolicy: {
 			href: '/legal/privacy-policy',
-			name: '隐私政策',
+			name: '個人情報保護方針',
 		},
 		CookiePolicy: {
 			href: '/legal/cookie-policy',
-			name: 'Cookie政策',
+			name: 'Cookieポリシー',
 		},
 	},
 	
 	routes: {
 		about: {
-			versions: '版本历史',
-			sponsors: '赞助商',
-			us: '关于我们',
+			versions: 'バージョン履歴',
+			sponsors: 'スポンサー',
+			us: '開発者について',
 		},
 		admin: {
-			home: '控制台',
+			home: 'コンソール',
 		},
 		home: '首页',
 		apps: {
-			chat: 'AI陪聊',
-			chatGPT: 'ChatGPT 聊天',
-			dalle: 'Dalle 作图',
+			chat: 'AIとのチャット',
+			chatGPT: 'ChatGPT チャット',
+			dalle: 'Dalle 画像を作成',
 		},
 		user: {
-			planning: '会员计划',
-			wall: '留言墙',
+			planning: '会員制度',
+			wall: 'メッセージウォール',
 		},
 		auth: {
-			home: '用户系统',
+			home: 'ユーザー',
 		},
 	},
 	notify: {
-		errorUserEmpty: '用户名不能为空！请先注册登录再使用！',
-		errorSendEmpty: '不能发送空消息！',
+		errorUserEmpty: 'ユーザー名を空欄にすることはできません。ご利用の前に、登録とログインをお願いします。',
+		errorSendEmpty: '空メッセージは送信できません。',
 	},
 	
 	ui: {
 		general: {
 			btn: {
-				send: '发送',
+				send: '送信',
 			},
 			textarea: {
-				placeholder: '在此处儿输入~',
+				placeholder: 'ここに入力してください。',
 			},
 		},
 		chat: {
 			btn: {
-				conversations: '会话历史',
+				conversations: '会話の記録',
 			},
 		},
 	},
 	
 	display: {
 		navs: {
-			services: '服务',
+			services: 'サービス',
 			settings: {
-				index: '设置',
-				language: '语言',
-				theme: '主题'
+				index: '設定',
+				language: '言語',
+				theme: 'テーマ'
 			},
-			about: '关于',
+			about: 'このサイトについて',
 			legal: '法律',
 		},
 	},
