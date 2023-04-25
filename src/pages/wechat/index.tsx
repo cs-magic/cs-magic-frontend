cc3235c830d8d2f549d8a33485dab612253729f7
markshawn2020 <shawninjuly@gmail.com>
2023-04-25 17:32:07 +0800

    enhanced wxid in frontend page

--- src/pages/wechat/index.tsx
+++ src/pages/wechat/index.tsx
@@ -4,21 +4,24 @@ import { CentralLayout } from '@/components/layouts/CentralLayout'
 import { CentralLoadingComp } from '@/components/general/CentralLoadingComp'
 import { QRCodeCanvas } from 'qrcode.react'
 import { Label } from '@/components/ui/label'
+import { skipToken } from '@reduxjs/toolkit/query'
 
 export const WechatPage = () => {
 	const router = useRouter()
 	
-	const { data } = useGetWechatQrcodeQuery(router.query.wxid as string | undefined)
+	const wxid = router.query.wxid as string | undefined
+	const { data } = useGetWechatQrcodeQuery(wxid ? wxid : skipToken)
 	
 	return (
 		<CentralLayout>
 			{
-				!data ? <CentralLoadingComp/>
-					: !data.success ? <p className={'bg-red-500 text-white'}>{data.content}</p>
-						: <div className={'flex flex-col gap-2 items-center'}>
-							<Label className={'text-xl'}>CS魔法社管理员登录</Label>
-							<QRCodeCanvas value={data.content} size={256} includeMargin/>
-						</div>
+				!wxid ? <p className={'bg-red-500 text-white'}>wxid is necessary</p>
+					: !data ? <CentralLoadingComp/>
+						: !data.success ? <p className={'bg-red-500 text-white'}>{data.content}</p>
+							: <div className={'flex flex-col gap-2 items-center'}>
+								<Label className={'text-xl'}>CS魔法社管理员登录</Label>
+								<QRCodeCanvas value={data.content} size={256} includeMargin/>
+							</div>
 			}
 		</CentralLayout>
 	)
