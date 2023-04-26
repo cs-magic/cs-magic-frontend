564146b9e6ca1d652ed614de475f76cd59667692
markshawn2020 <shawninjuly@gmail.com>
2023-04-26 17:44:12 +0800

    encapsulated qrcode comp

--- src/pages/about/contact.tsx
+++ src/pages/about/contact.tsx
@@ -1,21 +1,62 @@
 import { CentralLayout } from '@/components/layouts/CentralLayout'
-import { ContactView } from '@/components/contact/ContactView'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { AspectRatio } from '@radix-ui/react-aspect-ratio'
+import Image from 'next/image'
 
 export const AboutUsPage = () => {
+	
+	const ConstraintImage = ({ src, alt = 'img' }: { src: string, alt?: string }) => (
+		<AspectRatio ratio={9 / 12}>
+			<Image src={src} alt={alt} fill style={{ objectFit: 'contain' }} sizes={'320px'}/>
+		</AspectRatio>
+	)
+	
 	return (
 		<CentralLayout>
 			
-			{/*<Label>We Are: </Label>*/}
+			<Tabs defaultValue="test" className="w-[400px]">
+				
+				<TabsList className={'w-full justify-around'}>
+					<TabsTrigger value="test">加入内测群</TabsTrigger>
+					<TabsTrigger value="marketing">联系小助手</TabsTrigger>
+					<TabsTrigger value="commercial">商务咨询</TabsTrigger>
+					<TabsTrigger value="technology">技术咨询</TabsTrigger>
+				</TabsList>
+				
+				<TabsContent value="test">
+					<ConstraintImage src={'/qrcodes/wechat-test-group-0.3.jpeg'} alt={'test'}/>
+				</TabsContent>
+				
+				<TabsContent value="marketing">
+					<Tabs defaultValue={'east'}>
+						
+						<TabsList className={'w-full flex'}>
+							<TabsTrigger value={'east'}>亚太<br/>（北京时间 09 - 21）</TabsTrigger>
+							<TabsTrigger value={'west'}>美东<br/>（北京时间 21 - 09）</TabsTrigger>
+						</TabsList>
+						
+						<TabsContent value={'east'}>
+							<ConstraintImage src={'/qrcodes/wechat-clotho.png'} alt={'east marketing'}/>
+						</TabsContent>
+						
+						<TabsContent value={'west'}>
+							<ConstraintImage src={'/qrcodes/wechat-yyq.png'} alt={'west marketing'}/>
+						</TabsContent>
+					
+					</Tabs>
+				
+				</TabsContent>
+				
+				<TabsContent value="commercial">
+					<ConstraintImage src={'/qrcodes/wechat-susan-2.png'} alt={'commercial'}/>
+				</TabsContent>
+				
+				<TabsContent value="technology">
+					<ConstraintImage src={'/qrcodes/wechat-mark.jpeg'} alt={'technology'}/>
+				</TabsContent>
 			
-			{/*<Separator/>*/}
-			
-			{/*<Label>Sponsors:</Label>*/}
-			
-			{/*<Separator/>*/}
-			
-			<ContactView/>
+			</Tabs>
+		
 		</CentralLayout>
 	)
 }
--- src/pages/user-planning.tsx
+++ src/pages/user-planning.tsx
@@ -1,8 +1,125 @@
-import { UserPlanningPurchaseComp } from '@/components/userPlanning/UserPlanningPurchaseComp'
 import { RootLayout } from '@/components/layouts/RootLayout'
 import { useAppSelector } from '@/hooks/use-redux'
 import { selectU } from '@/states/features/i18nSlice'
 import { userPlanningPurchaseList } from '@/config/userPlanning'
+import { IUserPlanningPurchaseComp } from '@/ds/userPlanning'
+import { useState } from 'react'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import _ from 'lodash'
+import { Badge } from '@/components/ui/badge'
+import { AspectRatio } from '@radix-ui/react-aspect-ratio'
+import Image from 'next/image'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+import Link from 'next/link'
+import { routers } from '@/config/routers'
+import { Button } from '@/components/ui/button'
+
+
+export const UserPlanningPurchaseComp = (
+	{
+		name, prices, features, cover, tags, chatgptTokens,
+	}:
+		IUserPlanningPurchaseComp) => {
+	
+	const u = useAppSelector(selectU)
+	
+	const [byYear, setByYear] = useState(false)
+	
+	return (
+		
+		<Card className={'flex flex-col'}>
+			<CardHeader>
+				<div className={'w-full inline-flex items-center justify-between my-2'}>
+					<h2 className={'text-2xl font-bold'}>{_.startCase(_.camelCase(name)).split(' ').join('-').toUpperCase()}</h2>
+					<div className={'inline-flex items-center gap-2'}>
+						{tags.map((tag) => (
+							// <div key={tag} className="badge badge-secondary">{tag}</div>
+							<Badge key={tag}>{tag}</Badge>
+						))}
+					</div>
+				</div>
+			</CardHeader>
+			
+			<CardContent className={'grow'}>
+				<div className={'md:hidden'}>
+					<AspectRatio ratio={5 / 3}>
+						<Image src={cover} alt={name} fill priority sizes={'33vw'} className={'object-contain'}/>
+					</AspectRatio>
+				</div>
+				
+				<div className={'hidden md:block'}>
+					<AspectRatio ratio={9 / 12}>
+						<Image src={cover} alt={name} fill priority sizes={'33vw'} className={'object-cover'}/>
+					</AspectRatio>
+				</div>
+				
+				
+				<div className="grow">
+					<div className={'flex flex-col gap-2'}>
+						{/*<label className="cursor-pointer label">*/}
+						{/*	<span className="label-text">ChatGPT专属账号 <span className={'text-primary font-semibold text-lg'}>{chatgptTokens}w</span> token</span>*/}
+						{/*	<input type="checkbox" checked readOnly className={clsx(*/}
+						{/*		'checkbox checkbox-sm ',*/}
+						{/*		'checkbox-success',*/}
+						{/*	)}/>*/}
+						{/*</label>*/}
+						
+						<div className="flex items-center space-x-2">
+							<Checkbox checked/>
+							<Label>OpenAI专属账号 <span className={'text-primary font-semibold text-lg'}>{chatgptTokens}w</span> token</Label>
+						</div>
+						
+						{features.map((feature) => (
+							<div className="flex items-center space-x-2" key={feature.name}>
+								<Checkbox checked={feature.status === 'finished'}/>
+								<Label>{feature.name}</Label>
+							</div>
+							// <label className="cursor-pointer label" key={feature.name}>
+							// 	<span className="label-text">{feature.name}</span>
+							// 	<input type="checkbox" checked readOnly className={clsx(
+							// 		'checkbox checkbox-sm',
+							// 		feature.status === 'finished' && 'checkbox-success',
+							// 	)}/>
+							// </label>
+						))}
+						<label className="cursor-pointer label">
+							<span className="label-text">...</span>
+						</label>
+					</div>
+				
+				</div>
+			</CardContent>
+			
+			<CardFooter>
+				<div className={'w-full p-4 flex items-center justify-start gap-2'}>
+					{
+						prices.period ? (
+							<>
+								<p>按{byYear ? '年' : '月'}付费</p>
+								<Switch onCheckedChange={(checked) => {
+									setByYear(checked)
+								}}/>
+								<Label>￥{(byYear ? prices.period.year : prices.period.month) || '?'}</Label>
+							</>
+						) : (
+							<>
+								<p>按量付费</p>
+								<Label htmlFor={'price'}>￥{prices.quantity}</Label>
+							</>
+						)
+					}
+					
+					<Link href={routers.about.contactUS} className={'ml-auto'}>
+						<Button variant={'destructive'} size={null} className={'px-4 py-0'}>Buy</Button>
+					</Link>
+				</div>
+			</CardFooter>
+		</Card>
+	
+	)
+}
 
 
 export const UserPlanningPage = () => {
