78e2e899565d73ab13e5993dd081dd5b304d2975
markshawn2020 <shawninjuly@gmail.com>
2023-04-26 17:27:56 +0800

    added yyq qrcode page

--- src/components/contact/ContactView.tsx
+++ src/components/contact/ContactView.tsx
@@ -7,30 +7,49 @@ export const ContactView = () => {
 		<Tabs defaultValue="cmo" className="w-[400px]">
 			
 			<TabsList className={'w-full justify-around'}>
-				<TabsTrigger value="cmo">联系小助手</TabsTrigger>
 				<TabsTrigger value="test">加入内测群</TabsTrigger>
-				<TabsTrigger value="ceo">申请合作</TabsTrigger>
+				<TabsTrigger value="cmo">联系小助手</TabsTrigger>
+				<TabsTrigger value="ceo">技术咨询</TabsTrigger>
 			</TabsList>
 			
 			
-			<TabsContent value="test">
-				<AspectRatio ratio={9 / 12}>
-					<Image src={'/qrcodes/group-0.3.jpeg'} alt={'test'} fill style={{objectFit: 'contain'}} sizes={'320px'}/>
-				</AspectRatio>
+			<TabsContent value="cmo">
+				<Tabs defaultValue={'east'}>
+					
+					<TabsList className={'w-full flex'}>
+						<TabsTrigger value={'east'}>亚太<br/>（北京时间 09 - 21）</TabsTrigger>
+						<TabsTrigger value={'west'}>美东<br/>（北京时间 21 - 09）</TabsTrigger>
+					</TabsList>
+					
+					<TabsContent value={'east'}>
+						<AspectRatio ratio={9 / 12}>
+							<Image src={'/qrcodes/wechat-clotho.png'} alt={'CMO-EAST'} fill style={{ objectFit: 'contain' }} sizes={'320px'}/>
+						</AspectRatio>
+					</TabsContent>
+					
+					<TabsContent value={'west'}>
+						<AspectRatio ratio={9 / 12}>
+							<Image src={'/qrcodes/wechat-yyq.png'} alt={'CMO-WEST'} fill style={{ objectFit: 'contain' }} sizes={'320px'}/>
+						</AspectRatio>
+					</TabsContent>
+				
+				</Tabs>
+			
 			</TabsContent>
 			
-			<TabsContent value="cmo">
+			<TabsContent value="test">
 				<AspectRatio ratio={9 / 12}>
-					<Image src={'/qrcodes/wechat-clotho.png'} alt={'CMO'} fill style={{objectFit: 'contain'}} sizes={'320px'}/>
+					<Image src={'/qrcodes/group-0.3.jpeg'} alt={'test'} fill style={{ objectFit: 'contain' }} sizes={'320px'}/>
 				</AspectRatio>
 			</TabsContent>
 			
 			<TabsContent value="ceo">
 				<AspectRatio ratio={9 / 12}>
-					<Image src={'/qrcodes/wechat-mark.jpeg'} alt={'CEO'} fill style={{objectFit: 'contain'}} sizes={'320px'}/>
+					<Image src={'/qrcodes/wechat-mark.jpeg'} alt={'CEO'} fill style={{ objectFit: 'contain' }} sizes={'320px'}/>
 				</AspectRatio>
 			</TabsContent>
-			
+		
+		
 		</Tabs>
 	
 	)
