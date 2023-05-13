1228b283aa3d629e675fcb20d1229aa96899107b
markshawn2020 <shawninjuly@gmail.com>
2023-05-13 09:42:56 +0800

    UI: changed to support click on project item

--- src/components/home/ProjectView.tsx
+++ src/components/home/ProjectView.tsx
@@ -15,27 +15,33 @@ export const ProjectView = (
 	}: IProjectItem) => {
 	
 	return (
-		<Card className="card w-full md:w-[540px]">
-			
-			<CardHeader>
-				<CardTitle>{name}</CardTitle>
-				<CardDescription>{desc}</CardDescription>
-			</CardHeader>
-			
-			<CardContent>
-				<AspectRatio ratio={16 / 9}>
-					<Image fill className="rounded-md object-cover" src={cover} alt={cover} sizes={'(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw'}/>
-				</AspectRatio>
-			</CardContent>
-			
-			<CardFooter className={'w-full inline-flex items-center justify-between'}>
-				<div className={'inline-flex items-center gap-2'}>
-					{tags.map((tag) => (
-						<Badge key={tag}>{tag}</Badge>
-					))}
-				</div>
-				<Link href={href}><Badge variant={'destructive'} className={'py-0'}>Try</Badge></Link>
-			</CardFooter>
-		</Card>
+		<Link href={href}>
+			<Card className="card w-full md:w-[540px]">
+				
+				<CardHeader>
+					<CardTitle className={'inline-flex items-center justify-between'}>
+						{name}
+						
+						<div className={'inline-flex items-center gap-2'}>
+							{tags.map((tag) => (
+								<Badge key={tag}>{tag}</Badge>
+							))}
+						</div>
+					</CardTitle>
+					
+					<CardDescription>{desc}</CardDescription>
+				</CardHeader>
+				
+				<CardContent>
+					<AspectRatio ratio={16 / 9}>
+						<Image fill className="rounded-md object-cover" src={cover} alt={cover} sizes={'(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw'}/>
+					</AspectRatio>
+				</CardContent>
+				
+				<CardFooter className={'w-full inline-flex items-center justify-between'}>
+					{/*<Link href={href}><Badge variant={'destructive'} className={'py-0'}>Try</Badge></Link>*/}
+				</CardFooter>
+			</Card>
+		</Link>
 	)
 }
