deb862e6236dbbec76c51a3b92af633640651ca1
markshawn2020 <shawninjuly@gmail.com>
2023-06-19 18:34:03 +0800

    updated

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,6 +1,6 @@
 export enum AuthWay {
 	byBackend,
-	byNextAuth
+	byNextAuth, // NextAuth暂时废弃，原因是国内的登录服务与NextAuth不太兼容，手动实现email verification也比较麻烦
 }
 
 export const AUTH_WAY: AuthWay = AuthWay.byBackend
