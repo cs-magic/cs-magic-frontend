b8e0e0df5fcdc40edb2e554feb4f6543393f8584
markshawn2020 <shawninjuly@gmail.com>
2023-07-09 19:50:23 +0800

    [MVP] fixed database

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,7 @@ export const BACKEND_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT
 
 
 export const DATABASE_AUTH_DB_NAME = process.env.DATABASE_AUTH_DB_NAME
-export const DATABASE_MONGODB_URI = process.env.DATABASE_MONGODB_URI
+export const DATABASE_MONGO_URI = process.env.DATABASE_MONGO_URI
 
 export const EMAIL_SERVER = process.env.EMAIL_SERVER
 export const EMAIL_FROM = process.env.EMAIL_FROM
